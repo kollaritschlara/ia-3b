7f1aaba2b2f56e36cf74f7d87cedf7b6f0ca1313
kollaritschlara <141366195+kollaritschlara@users.noreply.github.com>
2025-05-23 11:12:57 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+        alternativas: ["alternativa1", "altrnativas2"]
+    }
+]
+{
+        enunciado: "Pergunta2",
+        alternativas: ["alternativa1", "altrnativas2"]
+}
